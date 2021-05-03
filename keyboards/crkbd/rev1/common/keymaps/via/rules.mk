2c422897776ca5af0fad2b2df09da8106f2699b0
Jonathan Haylett <jonathan@haylett.dev>
2021-05-03 15:37:09 +0100

    Add VIA config

--- keyboards/crkbd/rev1/common/keymaps/via/rules.mk
+++ keyboards/crkbd/rev1/common/keymaps/via/rules.mk
@@ -1,5 +1,6 @@
-MOUSEKEY_ENABLE     = no     # Mouse keys
+MOUSEKEY_ENABLE     = yes    # Mouse keys
+EXTRAKEY_ENABLE     = yes    # Media keys etc
 RGBLIGHT_ENABLE     = yes    # Enable WS2812 RGB underlight.
-VIA_ENABLE          = yes         # Enable VIA
+VIA_ENABLE          = yes    # Enable VIA
 OLED_DRIVER_ENABLE  = yes
 LTO_ENABLE          = yes
