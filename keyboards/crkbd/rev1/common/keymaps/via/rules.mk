3d00930957a326dc077c3c3521d71a1c91ac3ffc
Jonathan Haylett <jonathan@haylett.dev>
2021-05-15 23:29:13 +0100

    Fix failing to wake keyboard on resume

--- keyboards/crkbd/rev1/common/keymaps/via/rules.mk
+++ keyboards/crkbd/rev1/common/keymaps/via/rules.mk
@@ -1,6 +1,7 @@
 MOUSEKEY_ENABLE     = yes    # Mouse keys
 EXTRAKEY_ENABLE     = yes    # Media keys etc
-RGBLIGHT_ENABLE     = yes    # Enable WS2812 RGB underlight.
+RGBLIGHT_ENABLE     = yes    # Enable WS2812 RGB underlight
 VIA_ENABLE          = yes    # Enable VIA
 OLED_DRIVER_ENABLE  = yes
 LTO_ENABLE          = yes
+NO_USB_STARTUP_CHECK = yes   # Workaround for freezing after resume
